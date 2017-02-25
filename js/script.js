2721c82884b52634a993f27a222e320a783d5cfd
romaincinq <romaincinquilli@hotmail.fr>
2017-02-25 10:05:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,18 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function request(id,url){
+	var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("id").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "url", true);
+xmlhttp.send();
+}
+
+request(
