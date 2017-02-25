9e316e40d7f39b20e842c1a6183230878cf05c28
romaincinq <romaincinquilli@hotmail.fr>
2017-02-25 11:18:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,17 +17,4 @@ $(document).ready(function() {
 	});
 });
 
-function request(id,url){
-	var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("id").innerHTML = jsonPretty;
-    }
-};
-xmlhttp.open("GET", "url", true);
-xmlhttp.send();
-}
 
-request("bitcoin_block_number","http://bitcoin.mubiz.com/blockchaininfo");
